72f6e3e09404b291378faf5cfc46d31dd1223b88
Giovane Kauer <gfkauer@gmail.com>
2022-01-06 11:20:14 +0000

    Creating defult routes

--- api/test/integration/routes/products_spec.js
+++ api/test/integration/routes/products_spec.js
@@ -1,16 +1,17 @@
 describe('Routes: Products', () => {
     const defaultProduct = {
         name: 'Default product',
-        desctiption: 'product description',
+        description: 'product description',
         price: 100
     };
 
     describe('GET /products', () => {
         it('should return a list of products', done => {
+
             request
                 .get('/products')
                 .end((err, res) => {
-                    expect(res.body[0].to.eql(defaultProduct));
+                    expect(res.body[0]).to.eql(defaultProduct);
                     done(err);
                 });
         });
